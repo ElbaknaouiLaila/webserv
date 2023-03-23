1b8163ddfe927c461dc9d1d4a54e0d6c5b06bf3b
ElbaknaouiLaila <leilaelbaknaoui1999@gmail.com>
2023-03-23 15:07:14 +0000

    i have to check errors

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ std::string find_(std::map<std::string, std::string> header_map, std::string key
     for (; itr != header_map.end(); itr++)
     {
         std::string val = itr->second;
+        // std::cout<<itr->first<<" : "<<itr->second<<std::endl;
         if (itr->first == key)
             str_find = val;
     }
@@ -33,38 +34,63 @@ int hexToDigit(const std::string& hexStr) {
 }
 
 int main() {
+    // std::string httpRequest = "POST / HTTP/1.1\r\n"
+    //                           "Host: localhost:8080\r\n"
+    //                           "Connection: keep-alive\r\n"
+    //                           "Content-Length: 383\r\n"
+    //                           "Cache-Control: max-age=0\r\n"
+    //                           "sec-ch-ua-mobile: ?0\r\n"
+    //                           "sec-ch-ua-platform: \"macOS\"\r\n"
+    //                           "Upgrade-Insecure-Requests: 1\r\n"
+    //                           "Origin: http://localhost:8080\r\n"
+    //                           "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary1UdhAnnxETAnq932\r\n"
+    //                           "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36\r\n"
+    //                           "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n"
+    //                           "Sec-Fetch-Site: same-origin\r\n"
+    //                           "Sec-Fetch-Mode: navigate\r\n"
+    //                           "Sec-Fetch-User: ?1\r\n"
+    //                           "Sec-Fetch-Dest: document\r\n"
+    //                           "Referer: http://localhost:8080/form.html\r\n"
+    //                           "Accept-Encoding: gzip, deflate, br\r\n"
+    //                           "Accept-Language: en,fr-FR;q=0.9,fr;q=0.8,en-US;q=0.7,ar;q=0.6,fi;q=0.5\r\n"
+    //                           "\r\n"
+    //                           "------WebKitFormBoundary1UdhAnnxETAnq932\r\n"
+    //                           "Content-Disposition: form-data; name=comment\r\n"
+    //                           "Content-Type: application/octet-stream\r\n"
+    //                           "\r\n"
+    //                           "laila\r\n"
+    //                           "------WebKitFormBoundary1UdhAnnxETAnq932\r\n"
+    //                           "Content-Disposition: form-data; name=file; filename=""\r\n"
+    //                           "\r\n"
+    //                           "alila\r\n"
+    //                           "------WebKitFormBoundary1UdhAnnxETAnq932\r\n"
+    //                           "Content-Type: application/octet-stream\r\n"
+    //                           "------WebKitFormBoundary1UdhAnnxETAnq932--\r\n";
     std::string httpRequest = "POST / HTTP/1.1\r\n"
-                              "Host: localhost:8080\r\n"
+                              "Host: localhost:8000\r\n"
+                              "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:50.0) Gecko/20100101 Firefox/50.0\r\n"
+                              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+                              "Accept-Language: en-US,en;q=0.5\r\n"
+                              "Accept-Encoding: gzip, deflate\r\n"
                               "Connection: keep-alive\r\n"
-                              "Content-Length: 383\r\n"
-                              "Cache-Control: max-age=0\r\n"
-                              "sec-ch-ua-mobile: ?0\r\n"
-                              "sec-ch-ua-platform: \"macOS\"\r\n"
                               "Upgrade-Insecure-Requests: 1\r\n"
-                              "Origin: http://localhost:8080\r\n"
-                              "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary1UdhAnnxETAnq932\r\n"
-                              "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36\r\n"
-                              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n"
-                              "Sec-Fetch-Site: same-origin\r\n"
-                              "Sec-Fetch-Mode: navigate\r\n"
-                              "Sec-Fetch-User: ?1\r\n"
-                              "Sec-Fetch-Dest: document\r\n"
-                              "Referer: http://localhost:8080/form.html\r\n"
-                              "Accept-Encoding: gzip, deflate, br\r\n"
-                              "Accept-Language: en,fr-FR;q=0.9,fr;q=0.8,en-US;q=0.7,ar;q=0.6,fi;q=0.5\r\n"
+                              "Content-Type: multipart/form-data; boundary=---------------------------8721656041911415653955004498\r\n"
+                              "Content-Length: 465 \r\n"
                               "\r\n"
-                              "------WebKitFormBoundary1UdhAnnxETAnq932\r\n"
-                              "Content-Disposition: form-data; name=comment\r\n"
-                              "Content-Type: application/octet-stream\r\n"
+                              "-----------------------------8721656041911415653955004498\r\n"
+                              "Content-Disposition: form-data; name=myTextField\r\n"
                               "\r\n"
-                              "laila\r\n"
-                              "------WebKitFormBoundary1UdhAnnxETAnq932\r\n"
-                              "Content-Disposition: form-data; name=file; filename=""\r\n"
+                              "Test\r\n"
+                              "-----------------------------8721656041911415653955004498\r\n"
+                              "Content-Disposition: form-data; name=myCheckBox\r\n"
                               "\r\n"
-                              "alila\r\n"
-                              "------WebKitFormBoundary1UdhAnnxETAnq932\r\n"
-                              "Content-Type: application/octet-stream\r\n"
-                              "------WebKitFormBoundary1UdhAnnxETAnq932--\r\n";
+                              "on\r\n"
+                              "-----------------------------8721656041911415653955004498\r\n"
+                              "Content-Disposition: form-data; name=myFile; filename=test.txt\r\n"
+                              "Content-Type: text/plain\r\n"
+                              "\r\n"
+                              "Simple file.\r\n"
+                              "-----------------------------8721656041911415653955004498--\r\n";
     // std::string httpRequest = "POST /upload HTTP/1.1\r\n"
     //                           "Host: example.com\r\n"
     //                           "Content-Length: 100\r\n"
@@ -116,7 +142,7 @@ int main() {
     //------------------------ Parse the headers ------------------------//
     std::map<std::string, std::string> headersRequest;
     size_t prev_pos = 0;
-    while ((pos = header.find("\r\n", prev_pos)) != std::string::npos) {
+    while ((pos = header.find("\r\n", prev_pos)) != std::string::npos ) {
         std::string header_line = header.substr(prev_pos, pos - prev_pos);
         size_t colon_pos = header_line.find(':');
         if (colon_pos != std::string::npos) {
@@ -126,9 +152,19 @@ int main() {
         }
         prev_pos = pos + 2;
     }
+    pos = header.find("\r\n\r\n", prev_pos);
+        std::string header_line = header.substr(prev_pos, pos - prev_pos);
+        size_t colon_pos = header_line.find(':');
+        if (colon_pos != std::string::npos) {
+            std::string key = header_line.substr(0, colon_pos);
+            std::string value = header_line.substr(colon_pos + 1);
+            headersRequest[key] = value;
+        }
     //------------------------ Body of request------------------------//
-    size_t content_lenght;
-    content_lenght = stoi(find_(headersRequest, "Content-Length"));
+    // std::cout<<"here "<<std::endl;
+    // std::cout<<"Content-Length "<<find_(headersRequest, "Content-Length")<<std::endl;
+    
+    size_t content_lenght = stoi(find_(headersRequest, "Content-Length"));
     std::string body = httpRequest.substr(httpRequest.find(delimiter) + delimiter.length(), content_lenght);
     size_t pos_Bnd = find_(headersRequest, "Content-Type").find("boundary");
     size_t pos_Chnk = find_(headersRequest, "Transfer-Encoding").find("chunked");
@@ -143,20 +179,15 @@ int main() {
         while (std::getline(ss, line))
         {
             std::string boundary = "--" + boundary_str;
+                std::vector<std::string> headers;
             if(line.substr(0, boundary.size()) == boundary)
             {
-                std::vector<std::string> headers;
                 while (std::getline(ss, line) && line.size() != 1){
                     headers.push_back(line);
                 }
             }
-            while (std::getline(ss, line))
-            {
-                if (line.substr(0, boundary.size()) == boundary){
-                    break;
-                }
-                requestBody << line;
-            }
+                if (line != "\r")
+                    requestBody << line;
         }
     }else if(pos_Chnk != std::string::npos){
         //------------------------ parse chunked body ------------------------//
